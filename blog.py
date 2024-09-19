b3c603c90f641758b0e334224e153589c7c31749
perpetua-ona <onaperpetua@gmail.com>
2024-09-19 10:54:58 +0100

    Sample of template rendering

--- blog.py
+++ blog.py
@@ -3,7 +3,21 @@ from flask import Flask, render_template
 #create a flask instance
 app = Flask(__name__)
 
+# @app.route('/')
+
+# def index():
+#    return"<h1>Hi world</>"
+
 @app.route('/')
 
 def index():
-    return"<h1>Hi world</>"
+    return render_template ("index.html")
+
+@app.route('/user/<name>')
+
+def user(name):
+    return"<h1>Hi {}</>".format(name) 
+
+
+
+
